d45d4f4888787e04b99417caefdde2b2f1de9123
java-4fun <tsoltys@protonmail.com>
2024-09-24 13:41:52 +0200

    Main changed

--- Main.java
+++ Main.java
@@ -2,6 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        double liters;
+        int dist = 100;
+
+        minivan.passengers = 7;
+        minivan.fuelCap = 65;
+        minivan.lkm = 9.1;
+
+        sportscar.passengers = 2;
+        sportscar.fuelCap = 55;
+        sportscar.lkm = 12.5;
+
+        liters = minivan.fuelNeeded(dist);
+        System.out.println("Minivan needs " + liters + " to ride " + dist + ".");
+
+        liters = sportscar.fuelNeeded(dist);
+        System.out.println("Sportscar needs " + liters + " to ride " + dist + ".");
     }
 }
