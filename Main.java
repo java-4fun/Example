ea38b7438cf63d7aef75cf375a859ecbfec0e2f9
java-4fun <tsoltys@protonmail.com>
2024-09-24 15:53:14 +0200

    Constructor added

--- Main.java
+++ Main.java
@@ -2,24 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Vehicle minivan = new Vehicle();
-        Vehicle sportscar = new Vehicle();
+        Vehicle minivan = new Vehicle(7, 65, 9.1);
+        Vehicle sportscar = new Vehicle(2, 55, 12.5);
 
         double liters;
-        int dist = 100;
-
-        minivan.passengers = 7;
-        minivan.fuelCap = 65;
-        minivan.lkm = 9.1;
-
-        sportscar.passengers = 2;
-        sportscar.fuelCap = 55;
-        sportscar.lkm = 12.5;
+        int dist = 200;
 
         liters = minivan.fuelNeeded(dist);
-        System.out.println("Minivan needs " + liters + " to ride " + dist + ".");
+        System.out.println("Minivan needs " + liters + " to ride " + dist + " km.");
 
         liters = sportscar.fuelNeeded(dist);
-        System.out.println("Sportscar needs " + liters + " to ride " + dist + ".");
+        System.out.println("Sportscar needs " + liters + " to ride " + dist + " km.");
     }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -3,6 +3,11 @@ public class Vehicle {
     int fuelCap;
     double lkm;
 
+    Vehicle(int p, int f, double l) {
+        passengers = p;
+        fuelCap = f;
+        lkm = l;
+    }
     int range() {
         return (int) (fuelCap / lkm * 100);
     }
